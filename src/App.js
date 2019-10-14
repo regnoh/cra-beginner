86ba57d5442f182177986c6ce9bcb08b9edecba4
lixiaoru <lixiaoru@sensetime.com>
2019-10-14 17:45:41 +0800

    styles demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ClassComp from "./ClassComp";
 import FuncComp from "./FuncComp";
+import "./App.css";
+import styles from "./App.module.css";
+import "./App.scss"; // yarn add node-sass
 const App = () => {
   return (
-    <div>
-      <h1>hello from app</h1>
+    <div className="app">
+      <h1 className={styles.title}>hello from app</h1>
+      <p className="desc" style={{ margin: "2px" }}>
+        Have a good time!
+      </p>
       <ClassComp desc="hello from classComp" />
       <FuncComp desc="hello from funcComp" />
     </div>
